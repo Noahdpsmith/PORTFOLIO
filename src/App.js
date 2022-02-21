769621a31161f6f2945ee9feb47baa9ee171d613
noah smith <noahjosephsmith@gmail.com>
2022-02-21 13:26:35 -0700

    pushed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+// src/App.js
+
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-tsparticles';
+import particlesConfig from './config/particlesConfig';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ position: 'relative', overflow: "hidden" }}>
+      <div style={{ position: 'absolute'}}>
+        <Particles height="100vh" width="100vw" params={particlesConfig} />
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code></code>
         </p>
         <a
           className="App-link"
@@ -15,7 +23,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
