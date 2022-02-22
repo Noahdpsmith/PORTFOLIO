5ff1459497923fe2f453a7d61665a7b523f3098d
noah smith <noahjosephsmith@gmail.com>
2022-02-22 06:12:48 -0700

    port page

---
+++ src/components/Project.js
@@ -0,0 +1,80 @@
+// import React from "react";
+// import Project from "../components/Project";
+
+// const projects = [
+//   <div>
+//       id: 0,
+//     title: "Note Taker",
+//     languages: "html, css, js html, js, json, db, nodejs, heroku",
+//     packages: "node",
+//     image: "/img/notetaker.png",
+//     description: "A basic app that allows you to see the weather for a specific searched or saved city.",
+//     repo: "https://github.com/Noahdpsmith/Challenge-11",
+//     live: "https://note-taker-bby.herokuapp.com/notes.html",
+//   },
+    
+//   {
+//     id: 1,
+//     title: "Object Relational Mapping",
+//     languages: "js scheema, models APIs",
+//     packages: "sql mysql, express, insomnia",
+//     image: "/img/chal.png",
+//     description: "This is an application that uses a express.js API, sequelize to connect to mysql database. also shows GET, POST, DELETE and PUT routes that were tested with insomnia.",
+//     repo: "https://github.com/Noahdpsmith/challenge-13",
+//     live: "https://watch.screencastify.com/v/uTwtI0G3eaiUyKQQfURP",
+//   },
+//   {
+//     id: 2,
+//     title: "Quizam",
+//     languages: "html, css, js, handlebars, others",
+//     packages: ", connect-session-sequelize, dotenv, express, express-handlebars, express-session, handlebars, mysql2, puppeteer, sequelize",
+//     image: "/img/bookclub.png",
+//     description: "With the project we collaborated to create a app in which we could use to discuss books and other readable material using MySQL Handlebars Express Node HTML CSS Javascript Socket.IO",
+//     live: "https://agile-bastion-07410.herokuapp.com/",
+//     repo: "https://github.com/Noahdpsmith/BookWorm",
+//   },
+//   {
+//     id: 3,
+//     title: "covid 19 resources",
+//     languages: "html, css, js, api, databases,",
+//     packages: "nodejs",
+//     image: "/img/covid.png",
+//     description: "Application allows users to search Covid-19 testing sites within their zipcodes",
+//     repo: "https://github.com/Noahdpsmith/group10projecthttps://github.com/Jake-Roberts/group10project",
+//     live: "https://jake-roberts.github.io/group10project/",
+//   },
+//   {
+//     id: 4,
+//     title: "Basic Portfolio",
+//     languages: "html, css",
+//     packages: "none",
+//     image: "/img/portfolio.png",
+//     description: "A basic work portfolio that holds my projects for the full stack development class.",
+//     repo: "https://github.com/Noahdpsmith/Challenge-2",
+//     live: "https://noahdpsmith.github.io/Challenge-2/",
+//   },
+//   {
+//     id: 5,
+//     title: "Runbuddy",
+//     languages: "html, css, js",
+//     packages: "none",
+//     image: "/img/runbuddy.png",
+//     description: "A commissioned website for a team of people who want to help you find someone to run with",
+//     repo: "https://github.com/Noahdpsmith/runnin",
+//     live: "https://noahdpsmith.github.io/runnin/",
+//   },
+// ];
+// </div>
+
+// function Portfolio() {
+//   return (
+//     <div className="projects">
+      
+
+//       <Project projects={projects} />
+//     </div>
+//   );
+// }
+// }
+
+// export default Project;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,17 +1,90 @@
+
 import React from 'react';
 import '../App.css';
-import Hero from '../components/Hero';
-import Carosel from '../components/Carosel';
-import About from './About';
 
-const Portfolio = () => {
-  return(
-        <div>
-          <Hero></Hero>
-          <Carosel></Carosel>
-          
-          
-        </div>
-     );
+
+
+function Portfolio(props) {
+  return (
+   <div>
+       
+    <div>
+    <img alt="portfolio picture" className="photo above" src={require ('../images/portfolio.png')} />
+
+      <div className="firstcard above">
+                <h1>Note Taker</h1>
+            <p>Description: 
+                 A basic app that allows you to see the weather
+                 for a specific searched or saved city.",</p>
+            <a href='https://github.com/Noahdpsmith/Challenge-11'>github</a>
+            <a href='https://note-taker-bby.herokuapp.com/notes.html'>Live</a> 
+
+    </div>
+    <img alt="ORM picture" className="photo2 above" src={require ('../images/chal.png')} />
+
+    <div className="secondcard above">
+                <h1>ORM</h1>
+            <p>Description:
+                 This is an application that uses a express.js
+                  API, sequelize to connect to mysql database. 
+                  also shows GET, POST, DELETE and PUT routes 
+                  that were tested with insomnia.
+    </p>
+            <a href='https://github.com/Noahdpsmith/Challenge-11'>github</a>   
+            <a href='https://note-taker-bby.herokuapp.com/notes.html'>Live</a>  
+
+    </div>
+    <img alt="bookclub picture" className="photo3 above" src={require ('../images/bookclub.png')} />
+
+    <div className="thirdcard above">
+                <h1>bookclub</h1>
+            <p>Description: 
+                With the project we collaborated to create a app
+                 in which we could use to discuss books and other
+                  readable material using MySQL Handlebars Express
+                   Node HTML CSS Javascript Socket.IO
+    </p>
+            <a href='https://agile-bastion-07410.herokuapp.com/'>github</a>
+            <a href='https://github.com/Noahdpsmith/BookWorm'>Live</a>   
+
+    </div>
+    <img alt="covid picture" className="photo4 above" src={require ('../images/covid.png')} />
+
+    <div className="fourthcard above">
+                <h1>covid 19</h1>
+            <p>Description:
+                Application allows users to search Covid-19 testing
+                 sites within their zipcodes.</p>
+            <a href='https://github.com/Noahdpsmith/group10projecthttps://github.com/Jake-Roberts/group10project'>github</a>  
+            <a href='https://jake-roberts.github.io/group10project/'>Live </a>
+
+    </div>
+    <img alt="portfolio picture" className="photo5 above" src={require ('../images/portfolio.png')} />
+
+    <div className="fifthcard above">
+                <h1 className='above'>Portfolio start</h1>
+            <p className='above'>Description:
+                 A basic work portfolio that holds my projects for
+                  the full stack development class.</p>
+            <a className='above' href='https://github.com/Noahdpsmith/Challenge-2'>github</a>  
+            <a className='above' href='https://noahdpsmith.github.io/Challenge-2/'>Live</a> 
+
+    </div>
+    <img alt="Run Buddy" className="photo6 above" src={require ('../images/runbuddy.png')} />
+
+    <div className="sixthcard above">
+                <h1>Run Buddy</h1>
+            <p>Description: 
+            A commissioned website for a team of people who want
+            to help you find someone to run with,</p>
+            <a href='https://github.com/Noahdpsmith/runnin'>github</a> 
+            <a href='https://noahdpsmith.github.io/runnin/'>Live</a> 
+
+    </div>
+    </div>      
+</div>             
+  );
 }
+
+
 export default Portfolio;
